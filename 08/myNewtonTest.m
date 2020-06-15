512b025c8f01c743dbdb06a08394ae6bf74e6e11
Matthias Maile <matthias.maile@tu-dortmund.de>
2020-06-15 06:47:19 +0200

    bisection added

--- 08/myNewtonTest.m
+++ 08/myNewtonTest.m
@@ -5,8 +5,12 @@ f = @(x) (cos(2*x)).^2 - x.^2;
 df = @(x) -4 * cos(2*x) * sin(2*x) - 2 * x;
 
 x0 = 0.75;
+x00 = 0;
 
-[x,e,v] = myNewton(f, df, x0);
+[x1,e1,v1] = myNewton(f, df, x0);
+[x2,e2,v2] = mybisect(f, x00, x0);
 
-semilogy(e);
+semilogy(e1);
 hold on;
+semilogy(e2);
+
