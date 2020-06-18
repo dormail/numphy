f149b4312ec3020637e07c3ad59d367fcddb8a9b
Matthias Maile <matthias.maile@tu-dortmund.de>
2020-06-18 07:16:32 +0200

    arrays fixed

--- 08/myNewton.m
+++ 08/myNewton.m
@@ -1,17 +1,44 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2020     %%%
+%%%  Programmierabgaben (Praktischer Teil des Uebungungsblattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Student 1: Luca Daniel, Dietrich
+%  Unimail-adress: luca.dietrich@tu-dortmund.de
+%
+%  Student 2: Muhammed Tahir, Kamcili
+%  Unimail-adress: tahir.kamcili@tu-dortmund.de
+%
+%  Student 3: Matthias Werner Yarael, Maile
+%  Unimail-adress: matthias.maile@tu-dortmund.de
+%
+%  Student 4: Alfredo, Manente
+%  Unimail-adress: alfredo.manente@tu-dortmund.de
+%
+%
+% Uebungszettel-Nr: Blatt 8
+% Aufgabennummer:   8.1 a)
+% Program name:     myNewton.m
+%
+% Program(version): Octave
+% OS:               Fedora 32 WS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Die Funktion implementiert das Newtonverfahren
 function [x, e, v] = myNewton(f, df, x0)
 	e = 10^-12; % Genauigkeit
 
 	% rueckgabe werte
-	x(50) = 0; % vektor fuer iterierte
-	e(50) = 0; % vektor fuer fehler
-	v(50) = 0; % vektor fuer funtionswerte
+	x = []; % vektor fuer iterierte
+	e = []; % vektor fuer fehler
+	v = []; % vektor fuer funtionswerte
 
 	% startwert setzen
 	x(1) = x0;
+	x(2) = x0 - f(x0) / df(x0);
 
 	for i = 2:50
-		v(i) = f(x(i));
-		x(i) = x(i-1) + v(i-1) / df(x(i-1));
-		e(i) = abs(x(i) - x(i-1));
+		v = [v f(x(i))];
+		x = [x (x(i-1) + v(i-1) / df(x(i-1)))];
+		e = [e (abs(x(i) - x(i-1)))];
 	end
 end
--- 08/myNewtonTest.m
+++ 08/myNewtonTest.m
@@ -1,3 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2020     %%%
+%%%  Programmierabgaben (Praktischer Teil des Uebungungsblattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Student 1: Luca Daniel, Dietrich
+%  Unimail-adress: luca.dietrich@tu-dortmund.de
+%
+%  Student 2: Muhammed Tahir, Kamcili
+%  Unimail-adress: tahir.kamcili@tu-dortmund.de
+%
+%  Student 3: Matthias Werner Yarael, Maile
+%  Unimail-adress: matthias.maile@tu-dortmund.de
+%
+%  Student 4: Alfredo, Manente
+%  Unimail-adress: alfredo.manente@tu-dortmund.de
+%
+%
+% Uebungszettel-Nr: Blatt 8
+% Aufgabennummer:   8.1 b), c)
+% Program name:     myNewtonTest.m
+%
+% Program(version): Octave
+% OS:               Fedora 32 WS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Die Funktion testet das Newtonverfahren
 clear all;
 close all;
 
@@ -14,3 +40,8 @@ semilogy(e1);
 hold on;
 semilogy(e2);
 
+% zu c):
+% beim Newton verfahren erwarten wir eine immer schneller werdende Konvergenz (e < c * q^(2^n)), aufgrund der
+% quadratischen Konvergenz.
+% Daher steigt der graph hier auch an, während der des bisektionverfahrens weiter sinkt (aufgrund 
+% der linearen Konvergenz).
